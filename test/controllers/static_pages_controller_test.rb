1cedef0722108c4d28384a6f074bc3322bfbb5f8
Katsuhito Nakai <roisuandrois@hotmail.com>
2019-11-04 17:29:44 +0900

    3.4.2終了（演習前）

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "Atitle", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "Atitle", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "Atitle", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
